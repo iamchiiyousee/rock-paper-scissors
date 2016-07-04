3fe2e9e40b6c1a48be3c4ae3bbe20f36094a2cc5
Christopher Casasola <iamchiiyousee@yahoo.com>
2016-07-04 15:13:17 -0400

    stuff

--- RockPaperScissors/Program.cs
+++ RockPaperScissors/Program.cs
@@ -12,10 +12,11 @@ namespace RockPaperScissors
         {
             // TODO:
             // Add AIs by filling in lines like the ones below
-            //{ "Random 1", new RandomAI() },
+            { "Rambo the rando", new RandomAI() },
             //{ "Random 2", new RandomAI() },
             //{ "Other", new OtherAI() },
             //{ "YetAnother", new YetAnotherAI() },
+
         };
 
         static void Main(string[] args)
